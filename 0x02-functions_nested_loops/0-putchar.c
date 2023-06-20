2b1f74f1f9796f288640faa9a2da591e1823599f
JalalErr <jalil.errajy@gmail.com>
2023-06-20 14:12:10 -0700

    creating file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,12 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	char y[] = "_putchar";
@@ -6,10 +14,11 @@ int main(void)
 
 	for (j = 0; j <= 7; j++)
 	{
-		_putchar(y[j]);
+		_putchar(x[i]);
 	}
-		
+
 		_putchar('\n');
 
-	return 0;
+	return (0);
 }
+
