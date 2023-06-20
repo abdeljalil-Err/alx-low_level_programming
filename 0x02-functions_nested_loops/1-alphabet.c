65b9d3b58ea313a5334063f03306536bbba7a96f
JalalErr <jalil.errajy@gmail.com>
2023-06-20 14:54:46 -0700

    creating file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char ch;
+
+		for ( ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
