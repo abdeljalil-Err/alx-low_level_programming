2cafd0d3a429971c67320ffb6af6667e9c249300
JalalErr <jalil.errajy@gmail.com>
2023-06-20 18:19:22 -0700

    creating files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Check Main
+ * @a: integer variable.
+ * @b: integer variable.
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b.
+ */
+
+int add(int a, int b)
+{
+	int r;
+	r = a + b;
+	return r;
+}
+
