db63bff8a6f594b89746d95dc13b7207c258e7a5
JalalErr <jalil.errajy@gmail.com>
2023-06-20 18:44:06 -0700

    creating files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  main - check the code
- *
+ * main - check the code
+ * print_alphabet - prints the alphabe, in lowercase, followed by a new line
  * print_alphabet - dispalay alphabet, in lowercase.
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-		char ch;
+		har ch;
 
 		for (ch = 97; ch <= 122; ch++)
 		{
