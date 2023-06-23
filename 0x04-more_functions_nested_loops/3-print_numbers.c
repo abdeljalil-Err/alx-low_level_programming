e48ace4a48789122d954a5c1dc6df3e88e3e901c
JalalErr <jalil.errajy@gmail.com>
2023-06-22 17:39:51 -0700

    creating all files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	int q;
+
+	for (q >= 0; q <= 9; q++)
+	{
+		_putchar(q + '0');
+	}
+	_putchar('\n');
+}
