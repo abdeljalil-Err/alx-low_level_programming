b455d2ed8612c326e86dfa7112fae661f67dd6c2
JalalErr <jalil.errajy@gmail.com>
2023-06-22 17:17:19 -0700

    creating files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * mul - multipllication two integers
+ *
+ * @a: number 1
+ *
+ *@b: number 2
+ *
+ * Return: a * b
+ */
+
+
+
+int mul(int a, int b)
+{
+
+	int x;
+
+	x = a * b;
+	return (x);
+
+}
