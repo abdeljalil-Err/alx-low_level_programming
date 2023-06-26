9d51d472b8ed204dabcc029e6dc0af97496819ee
JalalErr <jalil.errajy@gmail.com>
2023-06-26 15:42:29 -0700

    editing file 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen - return lenght of a string.
- * @s: parametre input.
- * return: length of string.
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
 
 int _strlen(char *s)
