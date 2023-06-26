ae01ef842d6f60d79ce65bad4e0effb2743dd9e7
JalalErr <jalil.errajy@gmail.com>
2023-06-26 16:39:56 -0700

    file 4  edit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int stg;
+	
+	stg = 0;
+
+	for (i = 0; s[stg] != '\0';i++)
+	{
+		stg++;
+	}
+
+	for (i = stg--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	
+	_putchar('\n');
+}
