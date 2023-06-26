09a49bfc1b4e56a2922b41bbb9abb4137bb92e59
JalalErr <jalil.errajy@gmail.com>
2023-06-26 16:04:26 -0700

    editing file 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int k;
+	
+	k = 0;
+
+	while (str[k])
+	{
+		_putchar(str[k]);
+		k++;
+	}
+	_putchar('\n');
+}
