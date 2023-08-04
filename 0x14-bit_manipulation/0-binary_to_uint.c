835745bf731d35870105b49c50443f07da169cad
JalalErr <jalil.errajy@gmail.com>
2023-08-03 17:59:35 -0700

    editing file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int u;
-	int len, base_binary;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	u = 0;
+
+	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, base_binary = 1; len >= 0; len--, base_binary *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
@@ -28,9 +29,9 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[len] & 1)
 		{
-			i += base_binary;
+			ui += base_two;
 		}
 	}
 
-	return (u);
+	return (ui);
 }
