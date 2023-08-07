7f80f200013ead3612ccbaf5e8d6ccc1cdbde60c
JalalErr <jalil.errajy@gmail.com>
2023-08-07 11:35:41 -0700

    Editing files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
     char *buffer;
 
     if (filename == NULL)
-        return (0);
+        return 0;
     buffer = malloc(sizeof(char) * letters);
     if (buffer == NULL)
-        return (0);
+        return 0;
     opn = open(filename, O_RDONLY);
+    if (opn == -1)
+	    return 0;
     red = read(opn, buffer, letters);
-    wrt = write(opn, buffer, red);
-
+    if (red == -1)
+    {
+	    free(buffer);
+	    return 0;
+    }
+    wrt = write(STDOUT_FILENO, buffer, red);
+    if (wrt == -1)
+    {
+	    free(buffer);
+	    return 0;
+    }
     close(opn);
     free(buffer);
     return (wrt);   
